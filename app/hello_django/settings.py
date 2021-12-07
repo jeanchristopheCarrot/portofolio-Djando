29595e911e7eea9e8c16c659555ddbe232ac3dd6
Jean Carrot <jcc2usa@gmail.com>
2021-12-07 07:19:31 -0500

    using Django models

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'hello_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portofolio',
+        'USER': 'postgres',
+        'PASSWORD': 'new_password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
 }
 
 
